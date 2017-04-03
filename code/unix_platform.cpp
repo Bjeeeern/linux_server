32d22f26ccbc1c854015f364592cc562a2332ea7
Björn Strömberg <bjeeeern@gmail.com>
2017-04-03 19:57:20 +0200

    Fixes so the code compiles. Adds debugging with xcode. $> make debug

--- code/unix_platform.cpp
+++ code/unix_platform.cpp
@@ -1,22 +1,30 @@
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 #include "handmade.h"
 
 s32 main()
 {
-  int FileDescriptor;
-  FileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
+  int FileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
   if(FileDescriptor != -1)
   {
-    sockaddr Address = {};
+    struct sockaddr_in Address = {};
     //TODO(bjorn): Read this from a settings text file.
+    Address.sin_family = AF_INET;
     Address.sin_port = 8000;
-    Address.sin_ddr.s_addr = INADDR_ANY;
-    if(bind(FileDescriptor, &Address, sizeof(sockaddr)) == 0)
+    Address.sin_addr.s_addr = INADDR_ANY;
+    if(bind(FileDescriptor, (struct sockaddr *)&Address, sizeof(sockaddr)) == 0)
     {
+      if(listen(FileDescriptor, 10) == 0)
+      {
 
+      }
+      else
+      {
+        //TODO(bjorn): Log this.
+      }
     }
     else
     {
